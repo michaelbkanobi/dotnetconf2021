48689ae7794dbca6a1d25a1dd92cca6d05057b50
Michael B <texastechie@hotmail.com>
2021-11-10 22:23:09 -0600

    Add gRPC server project and add connected service to HelloConsole

--- HelloConsole/Program.cs
+++ HelloConsole/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Grpc.Net.Client;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Howdy, Y'all!");
+
+var channel = GrpcChannel.ForAddress("http://localhost:5263");
+var client  = new GrpcService79.Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new GrpcService79.HelloRequest { Name = "Dude" });
+Console.WriteLine("From gRPC server: " + reply.Message);
---
+++ HelloConsole/Readme.md
@@ -0,0 +1,6 @@
+﻿# gRPC service must be running to make the gRPC client call
++ Navigate to GrpcService79 folder and run dotnet run
++ Make sure hosted http url matches what the client code is pointing to
++ dotnet run the console app
+
+Note: greet.proto was imported to this console app by right-clicking dependencies > Manage Connected Services and Add Service Reference (gRPC) 
